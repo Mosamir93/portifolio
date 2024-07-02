00b73f92e7d215fdbd437e81141b9d250701e6e8
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-07-02 06:30:40 +0300

    final

--- shopping_advisor/scrapers/hyperone_scraper.py
+++ shopping_advisor/scrapers/hyperone_scraper.py
@@ -1,71 +1,36 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from ..browser_manager import BrowserManager
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import urllib.parse
 
 def scrape_hyperone(search_term):
-    opts = Options()
-    opts.add_argument("--headless")
-
-    # Initialize the browser
-    browser = Firefox(options=opts)
-
+    browser_manager = BrowserManager()
+    browser = browser_manager.get_browser()
     try:
         encoded_search_term = urllib.parse.quote(search_term)
-
-        # Construct the search URL
         search_url = f"https://www.hyperone.com.eg/search?q={encoded_search_term}&sortBy=relevance&sortOrder=DESC"
-        
-        # Open the search URL
         browser.get(search_url)
-        print(f"Page loaded successfully with search term '{search_term}'")
+        browser.find_elements(By.CLASS_NAME, "ProductTitle")
 
         try:
-            WebDriverWait(browser, 30).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "ProductTitle"))
-            )
-            print("Search results loaded")
-        except TimeoutException:
-            print(f"No results found for '{search_term}' on Hyperone")
-            return []
-
-        try:
-            no_result_svg = browser.find_element(By.CSS_SELECTOR, 'svg.icon.sprite-icons')
-            if no_result_svg:
-                print(f"No results found for '{search_term}' on Hyperone")
+            no_results_element = browser.find_elements(By.XPATH, "//svg[@data-v-66b7c9b4 and @data-v-42178517]")
+            if no_results_element:
                 return []
         except NoSuchElementException:
-            pass       
+            pass
 
-        # Get the updated page source after search
         html_content = browser.page_source
         soup = BeautifulSoup(html_content, 'html.parser')
-
-        # Find all product cards
         product_cards = soup.find_all('div', class_='flex items-baseline text-primary-700 font-bold mt-auto text-lg')
-
-        # List to hold product information
         products = []
 
         for card in product_cards:
-            # Extract the product name
             name_tag = card.find_previous('p', {'class': 'ProductTitle text-sm mt-2 text-system-gray w-full'})
             name = name_tag.get_text(strip=True) if name_tag else 'N/A'
-
-            # Extract the product price
             price_whole_tag = card.find('span', {'data-v-6750406f': ''})
             price_currency_tag = card.find('span', {'class': 'Currency ml-1'})
-
-            price_whole = price_whole_tag.get_text(strip=True) if price_whole_tag else 'N/A'
-            price_currency = price_currency_tag.get_text(strip=True) if price_currency_tag else 'N/A'
-            
-            price = f"{price_whole} {price_currency}"
-
-            # Extract the product image URL
+            price = f"{price_whole_tag.get_text(strip=True)} {price_currency_tag.get_text(strip=True)}"
             img_tag = card.find_previous('picture').find('img')
             img_url = img_tag['src'] if img_tag else 'N/A'
 
@@ -77,11 +42,8 @@ def scrape_hyperone(search_term):
             })
 
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except TimeoutException:
+        return []
     finally:
-        # Ensure the browser closes properly
-        browser.close()
-        browser.quit()
-        print("Browser closed")
+        browser_manager.close_browser()
     return products
--- shopping_advisor/scrapers/oscar_scraper.py
+++ shopping_advisor/scrapers/oscar_scraper.py
@@ -1,54 +1,33 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+from ..browser_manager import BrowserManager
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from bs4 import BeautifulSoup
 
 def scrape_oscar(search_term):
-    opts = Options()
-    opts.add_argument("--headless")
-    browser = Firefox(options=opts)
+    browser_manager = BrowserManager()
+    browser = browser_manager.get_browser()
 
     try:
         browser.get("https://www.oscarstores.com/products_search")
-
-        # Wait for search box to be present and then find it by ID
-        search_box = WebDriverWait(browser, 30).until(
-            EC.presence_of_element_located((By.ID, "search_input"))
-        )
-
+        search_box = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "search_input")))
         search_box.send_keys(search_term)
         search_box.submit()
-
-        try:
-            WebDriverWait(browser, 15).until(
+        WebDriverWait(browser, 30).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "card"))
             )
-        except TimeoutException:
-            print(f"No results found for '{search_term}' on Oscar")
-            return []       
 
-        # Get the updated page source after search
         html_content = browser.page_source
         soup = BeautifulSoup(html_content, 'html.parser')
-
-        # Find all product cards
         product_cards = soup.find_all('div', class_='card position-relative p-2 rounded-2 border-0 mx-1 w-100 h-100')
-
         products = []
 
         for card in product_cards:
             name_tag = card.find('h5', class_='my-2 one-lines ellipse text-left f_oswald c_black f-16 text-capitalize f-w_bold')
             name = name_tag.get_text(strip=True) if name_tag else 'N/A'
-
             price_tag = card.find('span', class_='c_red f-15')
-            price = price_tag.get_text(strip=True) if price_tag else 'N/A'
-
-            price = ' '.join(price.split())
-
+            price = ' '.join(price_tag.get_text(strip=True).split()) if price_tag else 'N/A'
             img_tag = card.find('img')
             img_url = img_tag['src'] if img_tag else 'N/A'
 
@@ -59,9 +38,8 @@ def scrape_oscar(search_term):
                 'vendor': 'Oscar'
             })
 
-
-
+    except TimeoutException:
+        return []
     finally:
-        browser.close()
-        browser.quit()
+        browser_manager.close_browser()
     return products
